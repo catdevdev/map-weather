c4fb9d067f187814f01895c650cc6e88d9c5faab
catdevdev <catdev.dev@gmail.com>
2021-01-21 22:42:38 +0200

    init map

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,67 @@
+import { useState, useCallback, useEffect } from "react";
 import Head from "next/head";
+import { GoogleMap, useJsApiLoader } from "@react-google-maps/api";
 
-export default function Home() {
+declare global {
+  interface Window {
+    google: any;
+  }
+}
+
+const center = {
+  lat: -3.745,
+  lng: -38.523,
+};
+
+const Index = ({ GOOGLE_MAP_KEY }) => {
+  const { isLoaded } = useJsApiLoader({
+    id: "google-map-script",
+    googleMapsApiKey: GOOGLE_MAP_KEY,
+  });
+
+  const [map, setMap] = useState(null);
+
+  const onLoad = useCallback(function callback(map) {
+    const bounds = new window.google.maps.LatLngBounds();
+    map.fitBounds(bounds);
+    setMap(map);
+  }, []);
+
+  const onUnmount = useCallback(function callback(map) {
+    setMap(null);
+  }, []);
+
   return (
     <div className="">
       <Head>
         <title>Map Weather</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      {isLoaded ? (
+        <GoogleMap
+          mapContainerStyle={{ width: "100wv", height: "100vh" }}
+          center={center}
+          zoom={10}
+          onLoad={onLoad}
+          onUnmount={onUnmount}
+        >
+          {/* Child components, such as markers, info windows, etc. */}
+          <></>
+        </GoogleMap>
+      ) : (
+        <></>
+      )}
     </div>
   );
+};
+
+export default Index;
+
+export async function getStaticProps() {
+  // Connect to Database using DB properties
+  return {
+    props: {
+      GOOGLE_MAP_KEY: process.env.GOOGLE_MAP_KEY,
+    },
+  };
 }
